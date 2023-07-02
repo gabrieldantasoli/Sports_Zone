14c160b8b64a30c6e80d5cb487a9a0075985fe70
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-07-02 16:26:48 -0300

    Criando estrutura de pastas e arquivos do Front_End.

---
+++ api/index.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+/**
+ *                       BACK_END
+ *                   ____     ______________            *                 
+ *     *            /___/|   /_____________/|                             
+ *                  |   ||   |    _________|/    *                     
+ *       *          |   ||   |    | |______                            
+ *   *              |   ||   |    |/______/|                                          
+ *       *   ___    |   ||   |________    ||         *            
+ *          /  /|___|   ||    ________|   ||
+ *    *    |  |_/___|   ||   /________|   ||
+ *          \__________//    |____________|/    *
+ * 
+ * 
+ *  + Developed by : GabrielDantasOli
+ *  + GITHUB       : https://github.com/gabrieldantasoli
+ *  + Contact      : gabriel.dantas.oliveira@ccc.ufcg.edu.br
+ *  + JAVASCRIPT FOR ALL LIFE!
+ * 
+ *   AUXILIAR : https://github.com/safak/youtube2022/tree/mern-booking
+ **/ 
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello, Sport Zone!!!
+    </div>
+  );
+}
+
+export default App;

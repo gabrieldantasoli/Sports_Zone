aad84f3260de22e28ca782967fdf07ae603e46fc
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-07-04 19:20:01 -0300

    Pass banner

--- client/src/components/passBanner/index.jsx
+++ client/src/components/passBanner/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tenis_img, Academia_img, Basquete_img, Corrida_img, Futebol_img, Piscinas_img } from '../../imgs';
 
 //IMPORTANDO O CSS
 import './pass.css';
@@ -7,7 +8,17 @@ export default () => {
 
     return (
         <section className='pass_banner'>
-            pass banner
+            <h2>Sport_zone Pass</h2>
+            <p>Aqui você tem acesso a varios benefícios com um único passe!</p>
+            <div className='images'>
+                <img src={Tenis_img} alt="Quadra de tênis" />
+                <img src={Academia_img} alt="Academia imagem" />
+                <img src={Basquete_img} alt="Quadra de basquete" />
+                <img src={Corrida_img} alt="Pista de corrida" />
+                <img src={Futebol_img} alt="Quadra de futebol" />
+                <img src={Piscinas_img} alt="Piscinas" />
+            </div>
+            <p>* Planos a partir de R$75 / Pessoa</p>
         </section>
     )
 }
--- client/src/imgs/index.js
+++ client/src/imgs/index.js
@@ -1,1 +1,7 @@
 export { default as Logo_img } from "./logo.png";
+export { default as Academia_img } from "./academia.jpg";
+export { default as Basquete_img } from "./basketball.jpg";
+export { default as Corrida_img } from "./corrida.jpg";
+export { default as Futebol_img } from "./futebol.jpg";
+export { default as Piscinas_img } from "./piscinas.jpg";
+export { default as Tenis_img } from "./tenis.jpg";

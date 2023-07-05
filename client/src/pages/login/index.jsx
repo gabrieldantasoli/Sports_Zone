3b9fe45833612a9391ee8329361a44379d000f04
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-07-05 20:48:00 -0300

    Login Html

---
+++ client/src/pages/login/index.jsx
@@ -0,0 +1,58 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+import { Logo_img } from "../../imgs";
+
+const Login = () => {
+const [credentials, setCredentials] = useState({
+username: undefined,
+password: undefined,
+});
+
+
+const navigate = useNavigate()
+
+const handleChange = (e) => {
+setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+};
+
+const handleClick = () => {
+
+}
+
+
+return (
+<div className="login">
+    <div className="lContainer">
+        <img src={Logo_img} alt="logo img" />
+        <label htmlFor="username">Email :</label>
+        <input
+            type="text"
+            placeholder="username"
+            id="username"
+            onChange={handleChange}
+            className="lInput"
+        />
+        <label htmlFor="password">Senha :</label>
+        <input
+            type="password"
+            placeholder="password"
+            id="password"
+            onChange={handleChange}
+            className="lInput"
+        />
+        <button disabled={false} onClick={handleClick} className="lButton">
+            Login
+        </button>
+        <p>Ao continuar, você concorda com as Condições de Uso da Sport Zone. Por favor verifique a Notificação de Privacidade, Notificação de Cookies e a Notificação de Anúncios Baseados em Interesse. </p>
+    {/* {error && <span>{error.message}</span>} */}
+    </div>
+    <div>
+        <span>Novo na Sport Zone ?</span>
+        <a href="/register">Criar minha conta</a>
+    </div>
+</div>
+);
+};
+
+export default Login;
--- client/src/routes/AppRoutes.js
+++ client/src/routes/AppRoutes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Home } from '../pages';
+import { Home, Login } from '../pages';
 
 export default () => {
     return(
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
         </Routes>
     );
 };

0376567d80a197597c33501432e724698f2c1874
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-07-06 09:40:47 -0300

    Starting Back_End

---
+++ client/src/pages/register/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//IMPORTANDO O CSS
+import '../login/login.css';
+
+export default () => {
+
+    return (
+        <div>
+            register   
+        </div>
+    )
+}
--- client/src/routes/AppRoutes.js
+++ client/src/routes/AppRoutes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Home, Login } from '../pages';
+import { Home, Login, Register } from '../pages';
 
 export default () => {
     return(
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
         </Routes>
     );
 };

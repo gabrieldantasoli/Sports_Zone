43558a197fb9804139ab84ad3a822b00cbb3cff9
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-07-15 13:25:46 -0300

    Cart products

--- client/src/pages/cart/index.jsx
+++ client/src/pages/cart/index.jsx
@@ -1,56 +1,75 @@
 import React, { useContext, useEffect, useState } from 'react';
-
-//IMPORTANDO O CSS
 import './cart.css';
 import { Header } from '../../components';
-import { json, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../context/authContext';
 import { Login_img } from '../../imgs';
+import { AuthContext } from '../../context/authContext';
 import { toast } from 'react-toastify';
 import axios from 'axios';
 
-
 export default () => {
+  const { user } = useContext(AuthContext);
 
-    const { user } = useContext(AuthContext);
+  const [products, setProducts] = useState([]);
+  const [linkProducts, setLinkProducts] = useState({});
 
-    const [products, setProducts] = useState([]);
-
-    const getUserCartProducts = async () => {
-        if (user != null) {
-            try {
-                const res = await axios.get(`/cart/${user._id}`);
-                setProducts(res.data)
-            } catch (err) {
-                toast.error(err.message);
-            }
-        }
+  const getUserCartProducts = async () => {
+    if (user != null) {
+      try {
+        const res = await axios.get(`/cart/${user._id}`);
+        setProducts(res.data);
+      } catch (err) {
+        toast.error(err.message);
+      }
     }
+  };
 
-    useEffect(() => {
-        getUserCartProducts();
-    }, []);
+  const getProducts = async () => {
+    let productsLink = {};
+    for (const item of products) {
+      try {
+        const res = await axios.get(`/product/${item.product_id}`);
+        productsLink[item.product_id] = res.data;
+      } catch (err) {
+        toast.error(err.message);
+      }
+    }
+    setLinkProducts(productsLink);
+  };
 
-    return (
-        <section className='cart'>
-            <Header />
-            { user ? (
-                <div className='shoppings'>
-                    
-                </div>
-            ) : (
-                <div >
-                    <div className="notLogged">
-                        <img src={Login_img} alt="gif login icon" />
-                        <div className="authenticate">
-                            <h3>You are not authenticated!</h3>
-                            <a href="/login">Login to see you cart.</a>
-                        </div>
-                        
-                    </div>
-                </div>
-                
-            )}
-        </section>
-    )
-}
+  useEffect(() => {
+    getUserCartProducts();
+  }, []);
+
+  useEffect(() => {
+    getProducts();
+  }, [products]);
+
+  return (
+    <section className="cart">
+      <Header />
+      {user ? (
+        <div className="shoppings">
+          {Object.keys(linkProducts).map((key) => {
+            const element = linkProducts[key];
+            console.log(element);
+            return (
+              <p key={key}>
+                {key}: {element.name}
+              </p>
+            );
+          })}
+        </div>
+      ) : (
+        <div>
+          <div className="notLogged">
+            <img src={Login_img} alt="gif login icon" />
+            <div className="authenticate">
+              <h3>You are not authenticated!</h3>
+              <a href="/login">Login to see your cart.</a>
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
